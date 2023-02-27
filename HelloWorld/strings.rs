8f668a2e6805e2d959312779879362eea11d7023
Reddeppa kollu <neerajchowdary889@gmail.com>
2023-02-27 15:37:34 +0530

    4th commit

--- HelloWorld/strings.rs
+++ HelloWorld/strings.rs
@@ -1,6 +1,28 @@
-fn main(){
-    let company:&str = "Microsoft";
-    let place:&str = "Hyderabad";
+static mut STRINGS: Option<String> = Some("Hello".to_string());
 
-    println!("Company is {}\nPlace is {}",company, place);
+fn main() {
+    // String literal
+    let company: &str = "Microsoft";
+    let place: &str = "Hyderabad";
+
+    println!("Company is {}\nPlace is {}", company, place);
+
+    unsafe {
+        // Mutable string
+        STRINGS = Some("Govindha".to_string());
+
+        let state: &'static str = "Telangana";
+        let cm: &'static str = "KCR";
+        println!("State is {}\nCM is {}", state, cm);
+
+        println!("Static string is {}", STRINGS.as_ref().unwrap());
+    }
+
+    not_main();
+}
+
+fn not_main() {
+    unsafe {
+        println!("Static string is {}", STRINGS.as_ref().unwrap());
+    }
 }
