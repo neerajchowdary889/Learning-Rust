202a29b49f403c81fba0f4676e362e8e5f09b19f
Reddeppa kollu <neerajchowdary889@gmail.com>
2023-03-01 01:14:28 +0530

    14th commit

--- HelloWorld/Arrays.rs
+++ HelloWorld/Arrays.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+use std::io;
 fn main(){
     let arr: [u8; 5] = [1,2,3,4,5];
     println!("{:?} ---> {}", arr, arr.len());
@@ -9,4 +11,25 @@ fn main(){
     println!("Array index 4 --> {}",array[3]);
     array[3] = "BCE";
     println!("Array index 4 --> {}",array[3]);
+    vectors();
+}
+
+fn vectors(){
+
+    let mut vector: Vec<i64> = vec![1,2,3,4,5];
+    println!("{:?}",vector);
+    vector.push(6);
+    println!("6 pushed to vector");
+
+    println!("Type you input:");
+    let mut input = String::new(); 
+    io::stdin().read_line(&mut input).expect("Wrong input");
+    let vec: i64 = input.trim().parse().expect("Something Wrong");
+
+    vector.push(vec);
+    println!("{:?}",vector);
+    
+    println!("\n Removing 6 from list");
+    vector.remove(5);
+    println!("{:?}",vector);
 }
