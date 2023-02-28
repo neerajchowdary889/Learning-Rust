bd89af5b6689ca54d137e4b9180fbee75238b160
Reddeppa kollu <neerajchowdary889@gmail.com>
2023-02-28 22:25:03 +0530

    11th commit

---
+++ HelloWorld/loops.rs
@@ -0,0 +1,65 @@
+fn main(){
+    Loop();
+    whileloop();
+    forloop();
+    forloopvect();
+}
+
+fn Loop(){
+    //This is infinite loop.
+    let mut n: i32 = 1;
+
+    loop{
+
+        if n%2 == 0{
+            n = n+1;
+            continue;
+        }
+        
+        if n >15{
+            break;
+        }
+        println!("Loop function: {}",n);
+
+        n = n+1;
+    }
+}
+
+fn whileloop(){
+    let mut n: i32 = 1;
+    
+    while n<=15{
+
+        if n%2 == 0{
+            println!("whileloop function: {}",n);
+        }
+        else{
+            n = n+1;
+            continue;
+        }
+        n = n+1;
+    }
+}
+
+fn forloop(){
+
+    for i in 1..15{
+
+        if i%2 == 0{
+            println!("Forloop function: {}",i);
+        }
+        else{
+            continue;
+        }
+
+    }
+}
+
+fn forloopvect(){
+    let branches = vec!["AIE","CSE","ECE","EEE","ELC"];
+
+    for i in branches.iter(){
+        println!("Branch is {}",i);
+    }
+    println!("{:?}",branches);
+}
