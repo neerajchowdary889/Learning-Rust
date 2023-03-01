332fd3f22295d89eedd982177610552da4148e44
Reddeppa kollu <neerajchowdary889@gmail.com>
2023-03-01 14:47:52 +0530

    16th commit

--- HelloWorld/Errorhandling.rs
+++ HelloWorld/Errorhandling.rs
@@ -1,5 +1,9 @@
+use std::fs::File;
+use std::io;
+use std::io::Read;
 static mut key: i8 = 56;
 fn main(){
+    recoverable();
     assertt();
     panicmessages(); 
 }
@@ -19,5 +23,21 @@ fn assertt(){
         assert!(temp == key, "Something went wrong");
         // if we have temp == key then it will continue. else it throws 
         // that something went wrong error we typed.
+    }
+}
+
+fn recoverable() -> Result<(), io::Error>{
+    let f = File::open("Rough.txt");
+    
+    let mut f = match f{
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s){
+        Ok(_) => Ok(()),
+        Err(e) => Err(e),
     }
 }
